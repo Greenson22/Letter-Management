3d1c63ab8ea8fdbc9bf18b3237d5276229db2c6c
Greenson <greenson22022002@gmail.com>
2024-11-11 14:38:07 +0800

    refactor : menambahkan fitur untuk pemrosesan pada gambar dengan ekstensi 'webp'

--- pdf_converter.py
+++ pdf_converter.py
@@ -1,5 +1,7 @@
+import os 
 from tqdm import tqdm
 import img2pdf
+from PIL import Image
 from utility import separate_files_and_folders, file_sort, validate_directory
 
 class PDFConverter:
@@ -9,6 +11,7 @@ class PDFConverter:
 
      # membuat pdf dari folder
      def make_pdf_from_folder(self):
+          print('Membuat pdf dari file dalam folder...')
           folders = separate_files_and_folders(self.input_dir)['folders'] #mengambil daftar folder
           for folder in tqdm(folders):
                # mengganti nama file pdf yang sesuai
@@ -21,11 +24,20 @@ class PDFConverter:
 
      # membuat pdf dari file
      def make_pdf_from_files(self):
+          print('Membuat pdf dari file...')
           files = separate_files_and_folders(self.input_dir)['files'] #mengambil daftar files
           for f in tqdm(files):
+               # jika menemukan webp, maka ubah ke pdf
+               # Kemudian hapus file webp
+               if f.endswith('.webp'):
+                    image = Image.open(f).convert('RGB')
+                    image.save(f.replace('.webp', '.png'), 'png') # melakukan penyimpanan file png yang baru
+                    os.remove(f) # menghapus file webp
+                    f = f.replace('.webp', '.png') # merubah webp ke png
+
                # mengganti nama file pdf yang sesuai
                # mengubah file image menjadi pdf
-               filename = f.replace(self.input_dir+'/', '').replace(' ', '_masuk').replace('.png', '')+'.pdf'
+               filename = f.replace(self.input_dir+'/', '').replace(' ', '_masuk').replace('.png', '').replace('.jpg', '').replace('.jpeg', '')+'.pdf'
                validate_directory(self.output_dir) # mengecek jika folder ada
                with open(self.output_dir+'/'+filename, 'wb') as file:
                     file.write(img2pdf.convert([f]))
